5d30d5fc690d2b5ee3ed03b88751b845a3505bbd
LabStation <arcolaus@outlook.com>
2021-09-11 21:14:39 +0800

    change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello,world\n");
+        JFrame jf1=new JFrame("hello");
+        jf1.setBounds(100,100,800,600);
+        jf1.setVisible(true);
+        jf1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
