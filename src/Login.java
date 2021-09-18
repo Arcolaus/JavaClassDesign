0320449d79d6f6d547733ac7bf7f605d0ba93721
LabStation <arcolaus@outlook.com>
2021-09-18 16:50:45 +0800

    change

--- src/Login.java
+++ src/Login.java
@@ -4,13 +4,40 @@ import java.awt.*;
 public class Login extends JFrame {
     private JLabel ImageShow;
     private ImageIcon ImageShow_Icon;
+
+    private JPanel InfoField;
+    private JTextField usrfield;
+    private JPasswordField pwfield;
+
     private String username;
     private String password;
 
     public Login() {
+        this.setLayout(new FlowLayout(0,0,0));
         ImageShow_Icon = new ImageIcon("img/LoginCover.png");
+        ImageShow_Icon.setImage(ImageShow_Icon.getImage().getScaledInstance(260, 260,Image.SCALE_SMOOTH));
+
         ImageShow = new JLabel(ImageShow_Icon);
+        ImageShow.setIcon(ImageShow_Icon);
         this.add(ImageShow);
-        ImageShow.setVisible(true);
+
+        InfoField=new JPanel();
+        InfoField.setLayout(new BorderLayout());
+        JLabel jl1=new JLabel("Username");
+        InfoField.add(jl1,BorderLayout.NORTH);
+        usrfield=new JTextField();
+        pwfield=new JPasswordField("Password");
+//        InfoField.add(usrfield,BorderLayout.NORTH);
+        InfoField.add(pwfield,BorderLayout.SOUTH);
+        this.add(InfoField);
+
+
+        this.setTitle("Login");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(600,300);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+//        this.setBounds(200,200,600,300);
+        this.setVisible(true);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,6 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         Login window=new Login();
-        window.setTitle("chk");
-        window.setBounds(100,100,800,600);
-        window.setVisible(true);
-        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
     }
 }
