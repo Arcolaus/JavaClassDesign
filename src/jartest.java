92d0c785a6f73ca3555f20577b29c34c6d0dd068
LabStation <arcolaus@outlook.com>
2021-09-22 16:16:05 +0800

    test parse class

--- src/jartest.java
+++ src/jartest.java
@@ -1,5 +1,25 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.io.IOException;
+
 public class jartest {
-    public static void main(String[] args){
-        System.out.println("chk");
+    public static void main(String[] args) {
+        Document doc = null;
+        try {
+            doc = Jsoup.parse(new File("img/schedule.html"), "GBK");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        Elements classElment = doc.getElementsByClass("Center");
+        for (Element it : classElment) {
+            if(it.text().length()!=0)
+                System.out.println(it.text());
+        }
     }
 }
