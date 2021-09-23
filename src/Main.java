a79a4d0f8ac9cba7647fab5ecb7b28eb2bf04fb2
LabStation <arcolaus@outlook.com>
2021-09-23 20:25:40 +0800

    决定采用正则表达式解析课程信息

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,35 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import main.lessonparse.Lessoninfo;
+
+import java.io.PrintStream;
 import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Vector<Lessoninfo> t=new Vector<Lessoninfo>();
+        Vector<Lessoninfo> t = new Vector<Lessoninfo>();
         Document doc = null;
         try {
             doc = Jsoup.parse(new File("./img/schedule.html"), "GBK");
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        Elements id = doc.select("td[nowrap=nowrap]");
-//        for (Element it : id)
-//            System.out.println(it.text());
-//        Elements StudentClass = doc.select("td[align=right]");
-//        for (Element it : StudentClass)
-//            System.out.println(it.text());
-//        Lessoninfo x=new Lessoninfo("5-8","<<JAVA语言课程设计>>;1 博知楼506 王靖 1-16周 实验学时");
+        PrintStream ps = null;
+        try {
+            ps = new PrintStream("d:/info.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         Elements LessonElements = doc.getElementsByClass("Center");
         for (Element element : LessonElements) {
-            if (element.text().length() != 0) {
-//                t.add(new Lessoninfo(element.id(),element.text()));
+//            if (element.text().length() != 0) {
+//                System.setOut(ps);
                 System.out.println(element.id() + element.text());
-            }
+//            }
         }
     }
 }
--- src/main/lessonparse/Lessoninfo.java
+++ src/main/lessonparse/Lessoninfo.java
@@ -16,41 +16,6 @@ public class Lessoninfo {
     private Vector takeWeek;
 
     public Lessoninfo(String time, String otherInfo) {
-        takeWeek = new Vector();
-        String[] timeinfo=time.split("-");
-        String[] lessoninfo = otherInfo.split(";| ");
-
-        // name
-        lessoninfo[0]=lessoninfo[0].replace("<<","");
-        lessoninfo[0]=lessoninfo[0].replace(">>","");
-        this.lessonName=lessoninfo[0];
-
-        // time
-        this.timeBase=Integer.parseInt(timeinfo[0]);
-        this.timeCombine=Integer.parseInt(timeinfo[1]);
-
-        // serial
-        this.serial=Integer.parseInt(lessoninfo[1]);
-
-        // room palce
-        this.roomPlace+=lessoninfo[2];
-
-        // teacher
-        this.teacher=lessoninfo[3];
-
-        // take week
-        if(lessoninfo[4].contains("双周")){
-            String[] week = lessoninfo[4].split("-|双周");
-            for (int i = Integer.parseInt(week[0]); i <= Integer.parseInt(week[1]); i++)
-                takeWeek.add(i);
-        }else{
-            String[] week=lessoninfo[4].split(",");
-            for(int i=0;i<week.length;i++)
-                takeWeek.add(Integer.parseInt(week[i]));
-        }
     }
-
-    // TODO 1.课程有冲突 2.某些课程有理论课和实验课，但是放在了同一行 3.单双周
-    // 课程有冲突
 
 }
