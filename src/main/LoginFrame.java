4e833219360d6db24bc9fa203244be3d5569a4f2
LabStation <arcolaus@outlook.com>
2021-10-06 22:31:52 +0800

    完成登录界面和主界面的耦合，完成主界面右侧栏

--- src/main/LoginFrame.java
+++ src/main/LoginFrame.java
@@ -1,26 +1,28 @@
 package main;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LoginFrame extends JFrame implements ActionListener {
-    private String username = "admin";
-    private String password = "kkk";
+    private String username = "1";
+    private String password = "1";
 
+    private boolean emptyInfo;
     private JPanel userInfo;
     private JTextField userNameFild;
     private JPasswordField passwordField;
 
-    private JLabel decorateLabel;
     private ImageIcon decorateImg;
 
     private JRadioButton adminOP;
     private JRadioButton userop;
     private ButtonGroup opGrop;
 
-    JButton userLogin;
+    private JButton userLogin;
     private JButton exit;
 
     private boolean loginPermission = false;
@@ -32,7 +34,7 @@ public class LoginFrame extends JFrame implements ActionListener {
 
         // 添加左侧图片
         decorateImg = new ImageIcon("img/LoginFrame/decoration.png");
-        decorateLabel = new JLabel();
+        JLabel decorateLabel = new JLabel();
         decorateLabel.setIcon(decorateImg);
         decorateLabel.setBounds(0, 0, decorateImg.getIconWidth() - 40, decorateImg.getIconHeight());
         this.add(decorateLabel);
@@ -48,23 +50,61 @@ public class LoginFrame extends JFrame implements ActionListener {
         //  添加用户名显示和输入框
         Font infoFont = new Font("Microsoft Yahei UI", Font.PLAIN, 20);
         JLabel usernameInfo = new JLabel("用户名");
-        userNameFild = new JTextField("admin");
+        userNameFild = new JTextField("1");
         usernameInfo.setFont(infoFont);
         usernameInfo.setBounds(350, 70, 80, 80);
         userNameFild.setFont(infoFont);
         userNameFild.setBounds(430, 95, 180, 30);
+        userNameFild.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                String inputName = userNameFild.getText();
+                String inputpwd = String.valueOf(passwordField.getPassword());
+                if (password.equals(inputpwd) && username.equals(inputName))
+                    loginPermission = true;
+                if (inputName.equals("") || inputpwd.equals(""))
+                    emptyInfo = true;
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+            }
+        });
         this.add(userNameFild);
         this.add(usernameInfo);
 
 
         //  添加密码输入提示和输入框
         JLabel passwordInfo = new JLabel("密码");
-        passwordField = new JPasswordField("kkk");
+        passwordField = new JPasswordField("1");
         passwordInfo.setFont(infoFont);
         passwordInfo.setBounds(350, 120, 80, 80);
         passwordField.setBounds(430, 145, 180, 30);
         passwordField.setEchoChar('*');
         passwordField.setFont(infoFont);
+        passwordField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                String inputName = userNameFild.getText();
+                String inputpwd = String.valueOf(passwordField.getPassword());
+                if (password.equals(inputpwd) && username.equals(inputName))
+                    loginPermission = true;
+                if (inputName.equals("") || inputpwd.equals(""))
+                    emptyInfo = true;
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+            }
+        });
         this.add(passwordInfo);
         this.add(passwordField);
 
@@ -106,28 +146,25 @@ public class LoginFrame extends JFrame implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
+        String inputName = userNameFild.getText();
+        String inputpwd = String.valueOf(passwordField.getPassword());
         if (e.getSource() == userLogin) {
-
-            String inputName = userNameFild.getText();
-            String inputpwd = String.valueOf(passwordField.getPassword());
             if (inputName.equals("") || inputpwd.equals(""))
                 JOptionPane.showMessageDialog(this, "用户名和密码不可为空!");
-            else {
-                if (password.equals(inputpwd) && username.equals(inputName)) {
-//                    System.out.println("chk");
-                    MainFrame jf = new MainFrame();
-                    loginPermission = true;
-                    this.setVisible(false);
-                }else{
-                    JOptionPane.showMessageDialog(this, "用户信息错误，请检查用户名和密码，或者用户类型");
-                }
-            }
         } else if (e.getSource() == exit) {
             this.dispose();
         }
     }
 
-    public boolean getPermission(){
+    public boolean getPermission() {
         return loginPermission;
+    }
+
+    public JButton getUserLogin() {
+        return userLogin;
+    }
+
+    public boolean emptyUserInfo() {
+        return emptyInfo;
     }
 }
--- src/main/MainFrame.java
+++ src/main/MainFrame.java
@@ -1,35 +1,123 @@
 package main;
 
+import main.lessonparse.Student;
+
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicButtonUI;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MainFrame extends JFrame implements ActionListener{
+public class MainFrame extends JFrame implements ActionListener {
+    final static int STUDENT = 0;
+    final static int ADMIN = 1;
+    // 登录及用户信息
+    private LoginFrame loginFrame;
+    private boolean loginPermited;
+    private int userType;
+    private String userId;
+
+    // 信息区
+    private JPanel infoField;
+    private JLabel profilePhoto;
+    private ImageIcon imgPhoto;
+    private JLabel userInfo;
+
+    // 功能区
+    private JPanel funcField;
+    private JButton funcExit;
+    private JButton funcAddLesson;
+    private JButton funcModifyLesson;
+    private JButton funcDeleteLesson;
+
+
+    // 学生信息
+    private Student student;
+
     public static void main(String[] args) {
-       // MainFrame k = new MainFrame();
-        LoginFrame t = new LoginFrame();
-        //t.userLogin.addActionListener(k);
-//        k.addComponentListener(t.userLogin);
-//        System.out.println(t.getPermission());
+        MainFrame mainFrame = new MainFrame();
     }
 
     MainFrame() {
-        super("Class Vision");
-//        t=new LoginFrame();
-//        t.userLogin.addActionListener(this);
-//        this.addL
-//        t.addComponentListener(this);
+        this.setLayout(null);
+        this.getContentPane().setBackground(new Color(255, 255, 255));
+        loginFrame = new LoginFrame();
+        loginFrame.getUserLogin().addActionListener(this);
+
+        // 添加右侧信息功能区
+        infoField = new JPanel();
+        infoField.setLayout(null);
+        infoField.setBounds(0, 5, 130, 520);
+        infoField.setBackground(new Color(210, 209, 209, 255));
+
+        // 添加右上角头像
+        profilePhoto = new JLabel();
+        imgPhoto = new ImageIcon("img/MainFrame/otherPhoto.png");
+        if (userType == STUDENT)
+            imgPhoto = new ImageIcon("img/MainFrame/studentPhoto.png");
+        else if (userType == ADMIN)
+            imgPhoto = new ImageIcon("img/MainFrame/adminPhoto.png");
+        imgPhoto.setImage(imgPhoto.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH));
+        profilePhoto.setIcon(imgPhoto);
+        profilePhoto.setBounds(15, 0, 100, 100);
+        infoField.add(profilePhoto);
+
+        // 添加用户信息栏
+        Font userinfoFont = new Font("黑体", Font.PLAIN, 14);
+        userInfo = new JLabel();
+        userInfo.setFont(userinfoFont);
+        userInfo.setText("<html>班级:计科00班<br>学号:S000001</html>");
+        userInfo.setVerticalAlignment(JLabel.CENTER);
+        userInfo.setBounds(15, 105, 100, 40);
+        infoField.add(userInfo);
+
+        // 添加右侧按钮
+        Font buttonFont = new Font("黑体", Font.PLAIN, 16);
+        funcField = new JPanel();
+        funcField.setOpaque(false);
+        funcField.setLayout(new GridLayout(4, 1, 0, 5));
+        funcField.setBounds(15, 200, 100, 140);
+        funcExit = new JButton("退出");
+        funcAddLesson = new JButton("添加课程");
+        funcModifyLesson = new JButton("修改课程");
+        funcDeleteLesson = new JButton("删除课程");
+
+        funcAddLesson.setFont(buttonFont);
+        funcDeleteLesson.setFont(buttonFont);
+        funcExit.setFont(buttonFont);
+        funcModifyLesson.setFont(buttonFont);
+
+        funcAddLesson.setBackground(Color.GREEN);
+        funcModifyLesson.setBackground(Color.ORANGE);
+        funcDeleteLesson.setBackground(Color.RED);
+        funcExit.setBackground(Color.WHITE);
+        funcAddLesson.setUI(new BasicButtonUI());
+        funcModifyLesson.setUI(new BasicButtonUI());
+        funcDeleteLesson.setUI(new BasicButtonUI());
+        funcExit.setUI(new BasicButtonUI());
+
+        funcField.add(funcAddLesson);
+        funcField.add(funcModifyLesson);
+        funcField.add(funcDeleteLesson);
+        funcField.add(funcExit);
+        this.add(funcField);
+
+        this.add(infoField);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(800, 600);
+        this.setSize(1024, 768);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
-//        this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource().equals(new LoginFrame().userLogin)){
-            System.out.println("success");
-        }
+
+//        if (loginFrame.getPermission()) {
+        this.setVisible(true);
+        loginFrame.dispose();
+//        } else if (loginFrame.emptyUserInfo() && !loginFrame.getPermission()) {
+//            JOptionPane.showMessageDialog(this, "用户信息错误，请检查用户名和密码，或者用户类型");
+//        }
+
     }
 }
